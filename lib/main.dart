4eef7cab1085baf830cdc7735b1226e352629c1b
Jerry <fenturechance@gmail.com>
2020-05-16 16:18:42 +0800

    dynamic function optional parameter default

--- lib/main.dart
+++ lib/main.dart
@@ -34,51 +34,47 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 10, bottom: 30, left: 10, right: 20),
-            child: Image.network('https://cdn.pixabay.com/photo/2020/04/27/11/03/ranunkeln-5099319_1280.jpg')
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: AspectRatio(
-              aspectRatio: 1280 / 853,
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(
-                      'https://cdn.pixabay.com/photo/2020/05/06/12/01/compass-5137269_1280.jpg'
-                    )
-                  )
-                ),
-                child: Container(
-                  alignment: Alignment.bottomRight,
-                  margin: EdgeInsets.all(5.0),
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 8.0,
-                      color: Colors.white60
-                    )
-                  ),
-                  child: Text(
-                    'Container Demo',
-                    style: TextStyle(
-                      color: Colors.white60,
-                      fontWeight: FontWeight.w900,
-                      fontSize: 20.0
-                    )
-                  ),
-                ),
-              )
-            )
-          ),
-          // Container(
-          //   padding: EdgeInsets.all(10),
-          //   decoration: BoxDecoration(border: Border.all(width: 5, color: Colors.blue)),
-          //   child: Image.network('https://cdn.pixabay.com/photo/2020/05/06/12/01/compass-5137269_1280.jpg'),
-          // )
+          buildImageView('https://pixabay.com/get/53e1d04a4c5aa414f6da8c7dda79317d103edfed5a4c704c7d267dd5914ec45b_1280.jpg'),
+          buildImageView('https://pixabay.com/get/53e1d3404253ae14f6da8c7dda79317d103edfed5a4c704c7d267dd5914ec45b_1280.jpg')
         ],
       )
     );
+  }
+  //也可以寫dynamic
+  Widget buildImageView(String url, { String title = 'Container Demo' }) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(url),
+              fit: BoxFit.cover
+            )
+          ),
+          child: Container(
+            alignment: Alignment.bottomRight,
+            margin: EdgeInsets.all(5.0),
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 8.0,
+                color: Colors.white60
+              )
+            ),
+            child: Text(
+              title,
+              style: TextStyle(
+                color: Colors.white60,
+                fontWeight: FontWeight.w900,
+                fontSize: 20.0
+              )
+            ),
+          ),
+        )
+      )
+    );
+    
   }
 }
